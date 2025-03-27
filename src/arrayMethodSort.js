39a8613489463a48377c2525964db365e6654ac4
PArtCreate <143039892+partcreate@users.noreply.github.com>
2025-03-27 08:20:53 +0000

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,17 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
+  [].__proto__.sort2 = function (compareFunction) {
     // write code here
+    const clArray = [...this];
+
+    const sArray = clArray.toSorted(compareFunction);
+
+    for (let i = 0; i < clArray.length; i++) {
+      this[i] = sArray[i];
+    }
+
+    return this;
   };
 }
 
