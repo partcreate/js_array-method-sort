7203628a7e1dd2b1be6315bb2444e1f95ffe8b77
PArtCreate <143039892+partcreate@users.noreply.github.com>
2025-03-27 08:41:23 +0000

    Solution - Bubble Sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,13 +6,36 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
     // write code here
-    const clArray = [...this];
+    let swapped;
 
-    const sArray = clArray.toSorted(compareFunction);
+    do {
+      swapped = false;
 
-    for (let i = 0; i < clArray.length; i++) {
-      this[i] = sArray[i];
-    }
+      for (let i = 0; i < this.length - 1; i++) {
+        let comparsionResult;
+
+        if (compareFunction) {
+          comparsionResult = compareFunction(this[i], this[i + 1]);
+        } else {
+          if (String(this[i]) > String(this[i + 1])) {
+            comparsionResult = 1;
+          } else if (String(this[i]) < String(this[i + 1])) {
+            comparsionResult = -1;
+          } else {
+            comparsionResult = 0;
+          }
+        }
+
+        if (comparsionResult > 0) {
+          const temp = this[i];
+
+          this[i] = this[i + 1];
+          this[i + 1] = temp;
+
+          swapped = true;
+        }
+      }
+    } while (swapped);
 
     return this;
   };
